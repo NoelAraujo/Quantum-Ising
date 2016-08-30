e656e1fbffb8f6644cef572485c225dde06d894a
noelaraujo <noelaraujo@live.com>
2016-08-30 13:52:09 -0300

    Plot time behavior of 10 spins
    Many performance improvements still can be done

--- start.m
+++ start.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 
-N = 1000;
+N = 1500;
 modo = 'random'; % random|static
 
 
@@ -10,11 +10,18 @@ if strcmp('random',modo)
     distance = 1;
     pd = makedist('Uniform','lower',0,'upper',(N*distance));
     position = random(pd,[1 N]);
-    histogram(position)
+    figure(1);histogram(position)
+    % the mouse indices are 10 random points that I follow their behavior along the time
+    % I'm using the word "mouse" in reference to the mouse at biologic
+    % experiments done with mouses
+    mouse_idx = randi([1 N],[1 10]); 
+    
 elseif strcmp('static',modo)
     % Create N points over a line with fixed unit distance
     distance = 1;
-    position = distance:distance:(N*distance);    
+    position = distance:distance:(N*distance);
+    % all the results should be equal fot the static case
+    mouse_idx = randi([1 N],[1 10]); 
 else
     disp('!! Problems !!')
     disp('The end')
@@ -27,7 +34,7 @@ spin0 = 0.5*randsrc(1,N);
 d = 1; % dimension
 J = 1; 
 h = 0; % external field
-alpha = 0.45;
+alpha = 0.35;
 % ref: [Romain Bachelard, Michael Kastner]Universal Threshold for the Dynamical Behavior of Lattice Systems with Long-Range Interactions
 
 %% Formula 2
@@ -39,14 +46,20 @@ d_ij2 = (N*distance) - d_ij1; % total lenght - distance between particles
 d_ij = min(d_ij1,d_ij2); % In periodic boundary condition, I need the smallest distance.
 
 %% Simulation
-for t = linspace(0.01,1,100)
+% figure(2)
+mouse_points = zeros(100,10);
+y_t = zeros(1,100);
+time_counter = 1;
+for t = linspace(0.01,0.5,100)
     disp(t)
     spin = spin0; 
     
     if alpha >= d/2
         y=0*spin+0.5*exp(-(2^(1+d-2*alpha)*(pi)^(d/2) )/((2*alpha-d)*gamma(d/2))*(4*J*t/pi)^(d/alpha));
+        y_t(time_counter) = 0.5*exp(-(2^(1+d-2*alpha)*(pi)^(d/2) )/((2*alpha-d)*gamma(d/2))*(4*J*t/pi)^(d/alpha));
     else
         y=0*spin+0.5*exp(-(2^(5+2*alpha-d)*(pi)^(d/2-2) )/((d-2*alpha)*gamma(d/2))*(J^2*t^2)*(N)^(1-(2*alpha)/d));
+        y_t(time_counter) = 0.5*exp(-(2^(5+2*alpha-d)*(pi)^(d/2-2) )/((d-2*alpha)*gamma(d/2))*(J^2*t^2)*(N)^(1-(2*alpha)/d));
     end
     
     for i=1:N
@@ -57,10 +70,19 @@ for t = linspace(0.01,1,100)
         end
     end
     spin = spin*cos(2*h*t); % on the for-loops I've compute the multiplications of all cos(). Now I multiply by the initial spin value
-    stem(spin,'filled','LineStyle','none')
-    hold on
-    plot(position,y,'r')
-    plot(position,-y,'r')
-    hold off
-    drawnow
+    mouse_points(time_counter,:) = spin(mouse_idx);
+    time_counter = time_counter+1;
+%     figure(2);stem(spin,'filled','LineStyle','none')
+%     hold on
+%     figure(2);plot(position,y,'r')
+%     figure(2);plot(position,-y,'r')
+%     hold off
+%     drawnow
 end
+
+%%
+for ii=1:10
+    scatter(linspace(0.01,0.5,100), abs(mouse_points(:,ii)),'filled')
+    hold on
+end
+    plot(linspace(0.01,0.5,100), y_t,'-k','LineWidth',5)
